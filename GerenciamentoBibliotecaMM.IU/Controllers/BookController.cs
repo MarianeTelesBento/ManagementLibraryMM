5825733f368abe7ff4955cf5d0d792d0452f90b8
Mariane Teles <Marianebteles2021@hotmail.com>
2024-12-16 17:40:08 -0300

    Add project files.

---
+++ GerenciamentoBibliotecaMM.IU/Controllers/BookController.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+using ManagementLibraryMM.UI.Models;
+
+namespace ManagementLibraryMM.UI.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ILogger<BookController> _logger;
+        public BookController(ILogger<BookController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            BookRegister bookRegister = new BookRegister();
+
+            bookRegister.RegisterData(book.Title, book.Author, book.DatePublication);
+
+            return View();
+        }
+
+        public IActionResult LoadFromJson()
+        {
+            return View();
+        }
+        public IActionResult SaveToJson()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+
+    }
+}
---
+++ GerenciamentoBibliotecaMM.IU/Models/BookRegister.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ManagementLibraryMM.UI.Models
+{
+    public class BookRegister
+    {
+        public int RegisterData(string title, string author, DateTime datePublication)
+        {
+            int sucess = 0;
+            string connectionString = "Data Source=DESKTOP-FTCVI92\\SQLEXPRESS;Initial Catalog=Library;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+
+            SqlCommand insertComm = new SqlCommand("INSERT INTO Books values(@title, @author, @publicationYear)", conn);
+
+            insertComm.Parameters.AddWithValue("@title", title);
+            insertComm.Parameters.AddWithValue("@author", author);
+            insertComm.Parameters.AddWithValue("@publicationYear", datePublication);
+
+            sucess = insertComm.ExecuteNonQuery();
+
+            conn.Close();
+
+            return sucess;
+        }
+    }
+}
