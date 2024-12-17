84d9ab49aa2dd20af61bb245a7e99ef55d2c60c4
Mariane Teles <Marianebteles2021@hotmail.com>
2024-12-17 18:01:45 -0300

    Create a search class, need  change code

---
+++ ManagementLibraryMM.UI/Models/BookSearch.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ManagementLibraryMM.UI.Models
+{
+    public class BookSearch
+    {
+        public List<Book> SearchData(int id)
+        {
+            int sucess = 0;
+            string connectionString = "Data Source=DESKTOP-FTCVI92\\SQLEXPRESS;Initial Catalog=Library;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+
+            SqlCommand insertComm = new SqlCommand("SELECT * FROM Books", conn);
+
+            sucess = insertComm.ExecuteNonQuery();
+
+            conn.Close();
+
+            return sucess;
+        }
+    }
+}
